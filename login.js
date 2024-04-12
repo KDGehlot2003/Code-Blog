66ea8286486293b5371c5bac8c9fa123b81b57df
Neel Patel <neelp1478@gmail.com>
2024-04-12 23:15:04 +0530

    Sign-up page updated

--- login.js
+++ login.js
@@ -12,4 +12,18 @@ function togglePasswordVisibility() {
         eyeClosed.style.display = "block";
     }
 }
+function togglePasswordVisibility1() {
+    var passwordInput = document.getElementById("confirmPassword");
+    var eyeOpen = document.getElementById("eyeOpen-confirm");
+    var eyeClosed = document.getElementById("eyeClosed-confirm");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeOpen.style.display = "block";
+        eyeClosed.style.display = "none";
+    } else {
+        passwordInput.type = "password";
+        eyeOpen.style.display = "none";
+        eyeClosed.style.display = "block";
+    }
+}
 
