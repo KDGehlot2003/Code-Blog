c6c85527d964401e7151f67864c213f591f412f2
Aditya Gupta <121934082+Adityagupta930@users.noreply.github.com>
2024-05-03 18:30:24 +0530

    edit some javascript

--- js/home.js
+++ js/home.js
@@ -15,3 +15,32 @@ window.onclick=function(event){
         }
     }
 };
+
+const cardsections = document.querySelectorAll('.card-image');
+cardsections.forEach(function(cardsection) {
+    cardsection.addEventListener('mouseenter', function() {
+        
+        this.style.transition = 'transform 0.3s ease, box-shadow 0.3s ease';
+        this.style.transform = 'scale(1.05)';
+        
+        this.style.boxShadow = '0px 4px 8px rgba(0, 0, 0, 0.1)';
+    });
+
+    cardsection.addEventListener('mouseleave', function() {
+        
+        this.style.transition = 'transform 0.3s ease, box-shadow 0.3s ease';
+        this.style.transform = 'scale(1)';
+        this.style.boxShadow = 'none';
+    });
+});
+
+const underline_navbar = document.querySelectorAll('.home-navbar-links-li');
+underline_navbar.forEach(link => {
+    link.addEventListener('mouseenter', function() {
+        this.style.textDecoration = 'underline'; 
+    });
+
+    link.addEventListener('mouseleave', function() {
+        this.style.textDecoration = 'none';
+    });
+});
